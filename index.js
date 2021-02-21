c7bdbf93078c9e9ce8deae76c4e85b53fd358040
Daniel Duque <danielvduque@gmail.com>
2021-02-20 23:01:10 -0300

    first commit. Express structure, add routes, process body and calculate trilateration

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Environment
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Support post requests - json 
+app.use(bodyParser.json());
+
+const port = process.env.PORT;
+
+// Server
+app.listen(port, () => {
+    console.log(`ML solution listening at ${port} port`)
+});
+
+// Test endpoint
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Routes
+const routes = require('./routes/routes');
+app.use('/api', routes);
+
+// Catch 404 error
+app.use((req, res) => {
+    res.status(404).json({
+        error: `page ${req.originalUrl} not found`,
+        status: 404
+    });
+});
+
+module.exports = app;
---
+++ routes/routes.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+
+/*
+    https://gist.github.com/kdzwinel/8235348
+*/
+function calculateTrilateration(x1, y1, x2, y2, x3, y3, r1, r2, r3) {
+    const S = (Math.pow(x3, 2.) - Math.pow(x2, 2.) + Math.pow(y3, 2.) - Math.pow(y2, 2.) + Math.pow(r2, 2.) - Math.pow(r3, 2.)) / 2.0;
+    const T = (Math.pow(x1, 2.) - Math.pow(x2, 2.) + Math.pow(y1, 2.) - Math.pow(y2, 2.) + Math.pow(r2, 2.) - Math.pow(r1, 2.)) / 2.0;
+    const y = ((T * (x2 - x3)) - (S * (x2 - x1))) / (((y1 - y2) * (x2 - x3)) - ((y3 - y2) * (x2 - x1)));
+    const x = ((y * (y1 - y2)) - T) / (x2 - x1);
+
+    return { x, y };
+}
+
+function getLocations(distances) {
+    const x1 = parseFloat(process.env.KENOBI_X);
+    const y1 = parseFloat(process.env.KENOBI_Y);
+    const x2 = parseFloat(process.env.SKYWALKER_X);
+    const y2 = parseFloat(process.env.SKYWALKER_Y);
+    const x3 = parseFloat(process.env.SATO_X);
+    const y3 = parseFloat(process.env.SATO_Y);
+    const r1 = parseFloat(distances[0]);
+    const r2 = parseFloat(distances[1] ? distances[1] : 0);
+    const r3 = parseFloat(distances[2] ? distances[2] : 0);
+
+    return calculateTrilateration(x1, y1, x2, y2, x3, y3, r1, r2, r3);
+}
+
+function getMessage() {
+}
+
+router.post('/topsecret', (req, res) => {
+    let r1, r2, r3;
+
+    const satellites = req.body.satellites;
+    satellites.forEach(sat => {
+        switch (sat.name.toLowerCase()) {
+            case 'kenobi':
+                r1 = sat.distance;
+                break;
+            case 'skywalker':
+                r2 = sat.distance;
+                break;
+            case 'sato':
+                r3 = sat.distance;
+                break;
+            default:
+                break;
+        }
+    });
+
+    const distances = [r1, r2, r3];
+    console.log(distances);
+    const { x, y } = getLocations(distances);
+
+    res.status(200).json({
+        position: {
+            x: x,
+            y: y
+        },
+        message: 'test message'
+    });
+
+});
+
+module.exports = router;
